a9c1d2999fc572021f966c859efe754890c79cac
jhays <jhays@mit.edu>
2025-11-21 10:09:46 -0500

    run_algo.sh

--- run.sh
+++ run.sh
@@ -3,20 +3,16 @@
 #SBATCH --job-name=lda
 #
 #SBATCH --ntasks=1
-#SBATCH -p sched_mit_sloan_interactive_r8
+#SBATCH -p sched_mit_sloan_batch_r8
 #SBATCH -o "slurm-%a.out"
-#SBATCH -e "slurm-%a.out0V"
-#SBATCH --time=24:00:00
-#SBATCH --mem-per-cpu=100000
+#SBATCH -e "slurm-%a.out"
+#SBATCH --time=96:00:00
+#SBATCH --mem-per-cpu=64G
 #SBATCH --mail-type=ERROR,END
 #SBATCH --mail-user=jhays@mit.edu
-#SBATCH --array=0-50
+#SBATCH --array=0-49
 
 
 export JHAYS="/home/jhays"
-
-
-# mamba init
-conda activate lda
 python3 $JHAYS/lda-1/model_training.py -i $SLURM_ARRAY_TASK_ID
 
